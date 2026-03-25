4b62854df55292f5cd771b88d16538585044bd43
lillyasmi <pinipelillyasmi@gmail.com>
2026-03-26 00:52:31 +0530

    fix cookie settings for HTTPS

--- frontend/lib/auth.js
+++ frontend/lib/auth.js
@@ -1,26 +1,34 @@
-import Cookies from 'js-cookie';
-import api from './api';
+import Cookies from "js-cookie";
+import api from "./api";
 
 export const login = async (email, password) => {
-  const { data } = await api.post('/auth/login', { email, password });
-  Cookies.set('token', data.token, { expires: 7 });
+  const { data } = await api.post("/auth/login", { email, password });
+  Cookies.set("token", data.token, {
+    expires: 7,
+    secure: true,
+    sameSite: "None",
+  });
   return data.user;
 };
 
 export const register = async (name, email, password) => {
-  const { data } = await api.post('/auth/register', { name, email, password });
-  Cookies.set('token', data.token, { expires: 7 });
+  const { data } = await api.post("/auth/register", { name, email, password });
+  Cookies.set("token", data.token, {
+    expires: 7,
+    secure: true,
+    sameSite: "None",
+  });
   return data.user;
 };
 
 export const logout = () => {
-  Cookies.remove('token');
-  window.location.href = '/login';
+  Cookies.remove("token");
+  window.location.href = "/login";
 };
 
 export const getMe = async () => {
-  const { data } = await api.get('/auth/me');
+  const { data } = await api.get("/auth/me");
   return data.user;
 };
 
-export const isLoggedIn = () => !!Cookies.get('token');
+export const isLoggedIn = () => !!Cookies.get("token");
